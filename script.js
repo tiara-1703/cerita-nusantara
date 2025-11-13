f1a2658a20e1e132df126328e79c68856c1087e7
tiara-1703 <tp828714@gmail.com>
2025-11-13 11:49:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ Setelah rahasianya terbongkar, Nawang Wulan sedih dan kembali ke kayangan, menin
 
   "Bali": {
     judul: "Legenda Danau Batur",
-    gambar: "danau_batur.png",
+    gambar: "lake_batur.png",
     isi: `Dahulu kala, di Bali hidup seorang raksasa wanita bernama Kebo Iwa.
 Ia sangat kuat dan suka membantu penduduk, tetapi juga sering makan banyak karena tubuhnya besar.
 Suatu hari, raja dari kerajaan lain ingin menaklukkan Bali dan menipu Kebo Iwa dengan pura-pura membangun sumur besar.
